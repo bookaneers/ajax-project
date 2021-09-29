15684c83cc94f4d8ffc6e9929e78662d4e289037
Fernando Figueiredo <bookaneers@gmail.com>
2021-09-28 18:23:47 -0600

    Almost completed.

--- js/fights.js
+++ js/fights.js
@@ -22,10 +22,12 @@ function fights() {
       if (playersPicksObj.player1[fightsResults.playerOneIndex][fightsResults.weapon] >
         playersPicksObj.player2[fightsResults.playerTwoIndex + 10][fightsResults.weapon]) {
         winner = fightsResults.playerOne;
+        fightsResults.victoriesPlayerOne++;
       };
       if (playersPicksObj.player1[fightsResults.playerOneIndex][fightsResults.weapon] <
         playersPicksObj.player2[fightsResults.playerTwoIndex + 10][fightsResults.weapon]) {
         winner = fightsResults.playerTwo;
+        fightsResults.victoriesPlayerTwo++;
       };
       if (playersPicksObj.player1[fightsResults.playerOneIndex][fightsResults.weapon] ===
         playersPicksObj.player2[fightsResults.playerTwoIndex + 10][fightsResults.weapon]) {
@@ -40,11 +42,68 @@ function fights() {
         '\n' + 'WINNER : ' + winner);
     }
 
+    if (fightsResults.weapon === 'moves' || fightsResults.weapon === 'abilities') {
+      if (playersPicksObj.player1[fightsResults.playerOneIndex][fightsResults.weapon].length >
+        playersPicksObj.player2[fightsResults.playerTwoIndex + 10][fightsResults.weapon].length) {
+        winner = fightsResults.playerOne;
+        fightsResults.victoriesPlayerOne++;
+      };
+      if (playersPicksObj.player1[fightsResults.playerOneIndex][fightsResults.weapon].length <
+        playersPicksObj.player2[fightsResults.playerTwoIndex + 10][fightsResults.weapon].length) {
+        winner = fightsResults.playerTwo;
+        fightsResults.victoriesPlayerTwo++;
+      };
+      if (playersPicksObj.player1[fightsResults.playerOneIndex][fightsResults.weapon].length ===
+        playersPicksObj.player2[fightsResults.playerTwoIndex + 10][fightsResults.weapon].length) {
+        winner = "TIE GAME";
+      };
+
+      alert('Weapon choose: ' + fightsResults.weapon +
+        '\n' + fightsResults.playerOne + '\'s ' + fightsResults.weapon + ' = ' +
+        playersPicksObj.player1[fightsResults.playerOneIndex][fightsResults.weapon].length +
+        '\n' + fightsResults.playerTwo + '\'s ' + fightsResults.weapon + ' = ' +
+        playersPicksObj.player2[fightsResults.playerTwoIndex + 10][fightsResults.weapon].length +
+        '\n' + 'WINNER : ' + winner);
+    }
 
 
+    if (fightsResults.weapon === 'base exp') {
+      if (playersPicksObj.player1[fightsResults.playerOneIndex]['base_experience'] >
+        playersPicksObj.player2[fightsResults.playerTwoIndex + 10]['base_experience']) {
+        winner = fightsResults.playerOne;
+        fightsResults.victoriesPlayerOne++;
+      };
+      if (playersPicksObj.player1[fightsResults.playerOneIndex]['base_experience'] <
+        playersPicksObj.player2[fightsResults.playerTwoIndex + 10]['base_experience']) {
+        winner = fightsResults.playerTwo;
+        fightsResults.victoriesPlayerTwo++;
+      };
+      if (playersPicksObj.player1[fightsResults.playerOneIndex]['base_experience'] ===
+        playersPicksObj.player2[fightsResults.playerTwoIndex + 10]['base_experience']) {
+        winner = "TIE GAME";
+      };
 
-    if (fight === 20) {
-      console.log('game over')
+      alert('Weapon choose: ' + fightsResults.weapon +
+        '\n' + fightsResults.playerOne + '\'s ' + fightsResults.weapon + ' = ' +
+        playersPicksObj.player1[fightsResults.playerOneIndex]['base_experience'] +
+        '\n' + fightsResults.playerTwo + '\'s ' + fightsResults.weapon + ' = ' +
+        playersPicksObj.player2[fightsResults.playerTwoIndex + 10]['base_experience']+
+        '\n' + 'WINNER : ' + winner);
+    }
+
+
+    if (fight === 5) {
+      if (fightsResults.victoriesPlayerOne > fightsResults.victoriesPlayerTwo) {
+        alert('PLAYER ONE IS THE WINNER');
+      }
+      if (fightsResults.victoriesPlayerOne < fightsResults.victoriesPlayerTwo) {
+        alert('PLAYER TWO IS THE WINNER');
+      }
+      if (fightsResults.victoriesPlayerOne === fightsResults.victoriesPlayerTwo) {
+        alert('TIE GAME');
+      }
+      var $message = document.querySelector('#message');
+      $message.textContent = 'GAME OVER';
     } else {
       startFight();
     }
@@ -52,21 +111,22 @@ function fights() {
 
   function handlePlayerOne(event) {
     var fighter1 = document.querySelector(`#${event.target.id}C`).textContent;
-    console.log(`handlePlayerOne ${fighter1}`);
     document.querySelectorAll(`.${event.target.className}`).forEach(item => {
+      // item.classList.remove('hoverOn');
       item.removeEventListener('click', handlePlayerOne);
     });
     fightsResults.playerOne = fighter1;
     fightsResults.playerOneGridPosition = event.path[0].id;
     fightsResults.playerOneIndex = Number(event.path[0].id.slice(3));
+    document.querySelector(`#${fightsResults.playerOneGridPosition}`).src = 'images/placeholder-image-square.jpg';
     if (fight % 2 === 1) pickWeapon();
     else fightResult();
   }
 
   function handlePlayerWeapon(event) {
     var weapon = event.target.textContent;
-    console.log(`handlePlayerWeapon ${weapon}`);
     document.querySelectorAll(`.${event.target.className}`).forEach(item => {
+      // item.classList.remove('hoverOn');
       item.removeEventListener('click', handlePlayerWeapon);
     });
     fightsResults.weapon = weapon;
@@ -76,13 +136,14 @@ function fights() {
 
   function handlePlayerTwo(event) {
     var fighter2 = document.querySelector(`#${event.target.id}C`).textContent;
-    console.log(`handlePlayerTwo ${fighter2}`);
     document.querySelectorAll(`.${event.target.className}`).forEach(item => {
+      // item.classList.remove('hoverOn');
       item.removeEventListener('click', handlePlayerTwo);
     });
     fightsResults.playerTwo = fighter2;
     fightsResults.playerTwoGridPosition = event.path[0].id;
     fightsResults.playerTwoIndex = Number(event.path[0].id.slice(3));
+    document.querySelector(`#${fightsResults.playerTwoGridPosition}`).src = 'images/placeholder-image-square.jpg';
     if (fight % 2 === 1) fightResult();
     else pickWeapon();
   }
@@ -91,16 +152,18 @@ function fights() {
     var $message = document.querySelector('#message');
     $message.textContent = 'Player One! Pick your Pokemon fighter...';
     const $playerOneFighter = document.querySelectorAll('.playerOneFighter').forEach(item => {
+      // item.classList.add('hoverOn');
       item.addEventListener('click', handlePlayerOne);
     });
-
   }
+
   function pickWeapon() {
     var $message = document.querySelector('#message');
     $message.textContent = 'Player! Pick your weapon...';
     if (fight % 2 === 1) $message.textContent = 'Player One! Pick your weapon...';
     else $message.textContent = 'Player Two! Pick your weapon...';
     const $weapons = document.querySelectorAll('.weapons').forEach(item => {
+      // item.classList.add('hoverOn');
       item.addEventListener('click', handlePlayerWeapon);
     });
   }
@@ -109,6 +172,7 @@ function fights() {
     var $message = document.querySelector('#message');
     $message.textContent = 'Player Two! Pick your Pokemon fighter...';
     const $playerTwoFighter = document.querySelectorAll('.playerTwoFighter').forEach(item => {
+      // item.classList.add('hoverOn');
       item.addEventListener('click', handlePlayerTwo);
     });
   }
